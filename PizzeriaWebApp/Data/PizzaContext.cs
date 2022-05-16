afd3dfb8d6ad214dd58d50d0d2403eb87254ea46
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-05-16 15:03:12 +0200

    Classe per integrazione db

---
+++ PizzeriaWebApp/Data/PizzaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzeriaWebApp.Models;
+
+namespace PizzeriaWebApp.Data
+{
+    public class PizzaContext : DbContext
+    {
+
+        public DbSet<Pizza> Pizzas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+
+            optionBuilder.UseSqlServer("Data Source=localhost; Database=blog_pizza; Integrated Security= true");
+
+
+        }
+
+    }
+}
