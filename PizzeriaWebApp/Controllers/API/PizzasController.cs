f348e4354a4c6e9947f4f9e1b4f2b9fbd7ce3886
Emiliano Rossi <emiliano.rossi658@hotmail.com>
2022-05-19 14:42:00 +0200

    controller api con metodo get creato

---
+++ PizzeriaWebApp/Controllers/API/PizzasController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PizzeriaWebApp.Data;
+using PizzeriaWebApp.Models;
+
+namespace PizzeriaWebApp.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string? search)
+        {
+            List<Pizza> pizzas = new List<Pizza>();
+            using (PizzaContext db = new PizzaContext())
+            {
+
+                // LOGICA PER RICERCARE I POST CHE CONTENGONO NEL TIUOLO O NELLA DESCRIZIONE LA STRINGA DI RICERCA
+                if (search != null && search != "")
+                {
+                    pizzas = db.Pizzas.Where(pizze => pizze.nome.Contains(search) || pizze.descrizione.Contains(search)).ToList<Pizza>();
+                }
+                else
+                {
+                    pizzas = db.Pizzas.ToList<Pizza>();
+                }
+            }
+
+            return Ok(pizzas);
+        }
+
+    }
+}
